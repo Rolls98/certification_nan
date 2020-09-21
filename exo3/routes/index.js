bbe33feb13494cbb8e132621c1869cd4519732d5
Rolls98 <toureb16@gmail.com>
2020-09-21 19:49:12 +0200

    last_commit

--- exo3/routes/index.js
+++ exo3/routes/index.js
@@ -10,4 +10,12 @@ router.get("/", async function (req, res, next) {
   res.render("index", { articles: articles.data.data });
 });
 
+router.get("/blog/:id", async function (req, res, next) {
+  let article = await axios.get(
+    "http://localhost:3000/api/articles/get/" + req.params.id
+  );
+  console.log(article.data);
+  res.json({ article: article.data });
+});
+
 module.exports = router;
